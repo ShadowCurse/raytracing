bda6e2af8e4f09e79f58552cf6ba794d25ca92eb
ShadowCurse <egik132@gmail.com>
2021-05-12 23:05:46 +0000

    gradient

--- src/main.rs
+++ src/main.rs
@@ -3,15 +3,60 @@ use sdl2::keyboard::Keycode;
 use sdl2::pixels::PixelFormatEnum;
 use sdl2::rect::Rect;
 
-const IMAGE_WIDTH: u32 = 256;
-const IMAGE_HEIGHT: u32 = 256;
+mod ray;
+mod vec3;
+
+use ray::*;
+use vec3::*;
+
+const ASPECT_RATIO: f32 = 16.0 / 9.0;
+const SCREEN_WIDTH: u32 = 400;
+const SCREEN_HEIGHT: u32 = (SCREEN_WIDTH as f32 / ASPECT_RATIO) as u32;
+const BUFFER_LENGTH: u32 = SCREEN_WIDTH * SCREEN_HEIGHT * 3;
+const PITCH: u32 = SCREEN_WIDTH * 3;
+
+fn ray_color(ray: &Ray) -> Color {
+    let unit_direction = ray.direction.unit();
+    let t = 0.5 * (-unit_direction.y + 1.0);
+    Color::new(1.0, 1.0, 1.0) * (1.0 - t) + Color::new(0.5, 0.7, 1.0) * t
+}
+
+fn create_texture() -> Vec<u8> {
+    let viewport_height = 2.0;
+    let viewport_width = ASPECT_RATIO * viewport_height;
+    let focal_length = 1.0;
+
+    let origin = Point3::new(0.0, 0.0, 0.0);
+    let horizontal = Vec3::new(viewport_width, 0.0, 0.0);
+    let vertical = Vec3::new(0.0, viewport_height, 0.0);
+    let lower_left_corner =
+        origin - horizontal / 2.0 - vertical / 2.0 - Vec3::new(0.0, 0.0, focal_length);
+
+    let mut buffer = vec![0u8; BUFFER_LENGTH as usize];
+    for y in 0..SCREEN_HEIGHT {
+        for x in 0..SCREEN_WIDTH {
+            let u = x as f32 / (SCREEN_WIDTH - 1) as f32;
+            let v = y as f32 / (SCREEN_HEIGHT - 1) as f32;
+
+            let dir = lower_left_corner + horizontal * u + vertical * v - origin;
+            let r = Ray::new(origin, dir);
+            let color = ray_color(&r);
+
+            let offset = y as usize * PITCH as usize + x as usize * 3;
+            buffer[offset] = (255.999 * color.x) as u8;
+            buffer[offset + 1] = (255.999 * color.y) as u8;
+            buffer[offset + 2] = (255.999 * color.z) as u8;
+        }
+    }
+    buffer
+}
 
 pub fn main() -> Result<(), String> {
     let sdl_context = sdl2::init()?;
     let video_subsystem = sdl_context.video()?;
 
     let window = video_subsystem
-        .window("rust_raytracing", IMAGE_WIDTH, IMAGE_WIDTH)
+        .window("rust_raytracing", SCREEN_WIDTH, SCREEN_HEIGHT)
         .position_centered()
         .opengl()
         .build()
@@ -21,28 +66,15 @@ pub fn main() -> Result<(), String> {
     let texture_creator = canvas.texture_creator();
 
     let mut texture = texture_creator
-        .create_texture_streaming(PixelFormatEnum::RGB24, IMAGE_WIDTH, IMAGE_HEIGHT)
+        .create_texture_streaming(PixelFormatEnum::RGB24, SCREEN_WIDTH, SCREEN_HEIGHT)
         .map_err(|e| e.to_string())?;
 
-    let pitch = (IMAGE_WIDTH * 3) as usize;
-    let mut buffer = vec![0u8; (IMAGE_WIDTH * IMAGE_HEIGHT * 3) as usize];
-    for y in 0..IMAGE_HEIGHT {
-        for x in 0..IMAGE_WIDTH {
-            let r = x as f32 / (IMAGE_WIDTH - 1) as f32;
-            let g = y as f32 / (IMAGE_HEIGHT - 1) as f32;
-            let b: f32 = 0.25;
+    let buffer = create_texture();
+    texture.update(None, &buffer, PITCH as usize);
 
-            let ir = 255.999 * r;
-            let ig = 255.999 * g;
-            let ib = 255.999 * b;
-
-            let offset = y as usize * pitch + x as usize * 3;
-            buffer[offset] = ir as u8;
-            buffer[offset + 1] = ig as u8;
-            buffer[offset + 2] = ib as u8;
-        }
-    }
-    texture.update(None, &buffer, pitch);
+    canvas.clear();
+    canvas.copy(&texture, None, None)?;
+    canvas.present();
 
     let mut event_pump = sdl_context.event_pump()?;
 
@@ -57,9 +89,6 @@ pub fn main() -> Result<(), String> {
                 _ => {}
             }
         }
-        canvas.clear();
-        canvas.copy(&texture, None, None)?;
-        canvas.present();
     }
 
     Ok(())
