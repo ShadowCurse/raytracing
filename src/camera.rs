0c8730ab54c3f8e0fd3f4c9d702702464a6ecec2
ShadowCurse <egik132@gmail.com>
2021-05-13 12:58:56 +0000

    added camera

---
+++ src/camera.rs
@@ -0,0 +1,36 @@
+use crate::ray::Ray;
+use crate::vec3::*;
+use crate::vec3::*;
+
+#[derive(Debug, Default, Copy, Clone)]
+pub struct Camera {
+    pub origin: Point3,
+    pub lower_left_corner: Point3,
+    pub horizontal: Vec3,
+    pub vertical: Vec3,
+}
+
+impl Camera {
+    pub fn new(aspect_ratio: f32) -> Self {
+        let viewport_height = 2.0;
+        let viewport_width = aspect_ratio * viewport_height;
+        let focal_length = 1.0;
+
+        let origin = Point3::new(0.0, 0.0, 0.0);
+        let horizontal = Vec3::new(viewport_width, 0.0, 0.0);
+        let vertical = Vec3::new(0.0, viewport_height, 0.0);
+        let lower_left_corner =
+            origin - 0.5 * horizontal - 0.5 * vertical - Vec3::new(0.0, 0.0, focal_length);
+        Self {
+            origin,
+            lower_left_corner,
+            horizontal,
+            vertical,
+        }
+    }
+
+    pub fn get_ray(&self, u: f32, v: f32) -> Ray {
+        let dir = self.lower_left_corner + u * self.horizontal + v * self.vertical - self.origin;
+        Ray::new(self.origin, dir)
+    }
+}
