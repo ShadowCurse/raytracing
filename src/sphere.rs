5b32d0b11948ed12f6cafbfa8133b6664ae7b42e
ShadowCurse <egik132@gmail.com>
2021-05-27 17:50:53 +0000

    added moving sphere class

--- src/sphere.rs
+++ src/sphere.rs
@@ -54,3 +54,69 @@ impl Hittable for Sphere {
         ))
     }
 }
+
+pub struct MovingSphere {
+    pub center0: Point3,
+    pub center1: Point3,
+    pub time0: f32,
+    pub time1: f32,
+    pub radius: f32,
+    pub material: Arc<WithMaterialTrait>,
+}
+
+impl MovingSphere {
+    pub fn new(
+        center0: Point3,
+        center1: Point3,
+        time0: f32,
+        time1: f32,
+        radius: f32,
+        material: Arc<WithMaterialTrait>,
+    ) -> Self {
+        Self {
+            center0,
+            center1,
+            time0,
+            time1,
+            radius,
+            material,
+        }
+    }
+
+    pub fn center(&self, time: f32) -> Point3 {
+        self.center0
+            + ((time - self.time0) / (self.time1 - self.time0)) * (self.center1 - self.center0)
+    }
+}
+impl Hittable for MovingSphere {
+    fn hit(&self, ray: &Ray, t_min: f32, t_max: f32) -> Option<HitRecord> {
+        let os = ray.origin - self.center(ray.time);
+        let a = ray.direction.length_squared();
+        let half_b = os.dot(&ray.direction);
+        let c = os.length_squared() - self.radius.powi(2);
+
+        let discriminant = half_b.powi(2) - a * c;
+        if discriminant < 0.0 {
+            return None;
+        }
+        let sqrtd = discriminant.sqrt();
+
+        let mut root = (-half_b - sqrtd) / a;
+        if root < t_min || root > t_max {
+            root = (-half_b + sqrtd) / a;
+            if root < t_min || root > t_max {
+                return None;
+            }
+        }
+
+        let point = ray.at(root);
+        let outward_normal = (point - self.center(ray.time)) / self.radius;
+        Some(HitRecord::new(
+            point,
+            root,
+            self.material.borrow(),
+            &ray,
+            &outward_normal,
+        ))
+    }
+}
