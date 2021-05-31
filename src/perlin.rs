f50a868fbe22d20e018f6f3e3391cde2772e6aa4
ShadowCurse <egik132@gmail.com>
2021-05-31 21:27:32 +0000

    fixes

--- src/perlin.rs
+++ src/perlin.rs
@@ -1,6 +1,6 @@
 use crate::vec3::Point3;
 
-const PERLIN_POINT_COUNT : u32 = 255;
+const PERLIN_POINT_COUNT: u32 = 256;
 
 pub struct Perlin {
     random_flot: Vec<f32>,
@@ -16,9 +16,9 @@ impl Perlin {
         let uniform = rand::distributions::Uniform::new(0.0, 1.0);
 
         Self {
-            random_flot: (0..PERLIN_POINT_COUNT).map(|x| 
-                                uniform.sample(&mut rng)
-                             ).collect::<Vec<_>>(),
+            random_flot: (0..PERLIN_POINT_COUNT)
+                .map(|x| uniform.sample(&mut rng))
+                .collect::<Vec<_>>(),
             perm_x: Self::perlin_generate_perm(),
             perm_y: Self::perlin_generate_perm(),
             perm_z: Self::perlin_generate_perm(),
@@ -26,21 +26,21 @@ impl Perlin {
     }
 
     pub fn noise(&self, point: &Point3) -> f32 {
-        let i = (4.0 * point.x) as u32 & 255;
-        let j = (4.0 * point.y) as u32 & 255;
-        let k = (4.0 * point.z) as u32 & 255;
-        self.random_flot[self.perm_x[i] ^ self.perm_y[j] ^ self.perm_z[k]]
+        let i = (4.0 * point.x) as usize & 255;
+        let j = (4.0 * point.y) as usize & 255;
+        let k = (4.0 * point.z) as usize & 255;
+        self.random_flot[(self.perm_x[i] ^ self.perm_y[j] ^ self.perm_z[k]) as usize]
     }
 
     fn perlin_generate_perm() -> Vec<u32> {
-        let mut p = (0..PERLIN_POINT_COUNT).map(|x| x).collect::<Vec<u32>>(); //vec![0; PERLIN_POINT_COUNT as usize];
+        let mut p = (0..PERLIN_POINT_COUNT).map(|x| x).collect::<Vec<u32>>();
 
         use rand::distributions::Distribution;
         let mut rng = rand::thread_rng();
-        for i in (0..PERLIN_POINT_COUNT).rev() {
+        for i in (1..PERLIN_POINT_COUNT).rev() {
             let uniform = rand::distributions::Uniform::new(0, i);
             let target = uniform.sample(&mut rng);
-            std::mem::swap(p[i], p[target]);
+            p.swap(i as usize, target as usize);
         }
         p
     }
