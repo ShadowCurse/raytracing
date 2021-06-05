49951a0804201be949c72b71aa0be3620fe54992
ShadowCurse <egik132@gmail.com>
2021-06-05 14:30:49 +0000

    renamed from spheres, added XYRect

--- src/objects.rs
+++ src/objects.rs
@@ -154,3 +154,64 @@ impl Hittable for MovingSphere {
         Some(AABB::surrounding_box(box0, box1))
     }
 }
+
+pub struct XYRect {
+    pub x0: f32,
+    pub x1: f32,
+    pub y0: f32,
+    pub y1: f32,
+    pub k: f32,
+    pub material: Arc<WithMaterialTrait>,
+}
+
+impl XYRect {
+    pub fn new(
+        x0: f32,
+        x1: f32,
+        y0: f32,
+        y1: f32,
+        k: f32,
+        material: Arc<WithMaterialTrait>,
+    ) -> Self {
+        Self {
+            x0,
+            x1,
+            y0,
+            y1,
+            k,
+            material,
+        }
+    }
+}
+
+impl Hittable for XYRect {
+    fn hit(&self, ray: &Ray, t_min: f32, t_max: f32) -> Option<HitRecord> {
+        let t = (self.k - ray.origin.z) / ray.direction.z;
+        if t < t_min || t > t_max {
+            return None;
+        }
+        let x = ray.origin.x + t * ray.direction.x;
+        let y = ray.origin.y + t * ray.direction.y;
+        if x < self.x0 || x > self.x1 || y < self.y0 || y > self.y1 {
+            return None;
+        }
+        let u = (x - self.x0) / (self.x1 - self.x0);
+        let v = (y - self.y0) / (self.y1 - self.y0);
+        Some(HitRecord::new(
+            ray.at(t),
+            t,
+            u,
+            v,
+            self.material.borrow(),
+            &ray,
+            &Vec3::new(0.0, 0.0, 1.0),
+        ))
+    }
+
+    fn bounding_box(&self, _: f32, _: f32) -> Option<AABB> {
+        Some(AABB::new(
+            Point3::new(self.x0, self.y0, self.k - 0.00001),
+            Point3::new(self.x1, self.y1, self.k + 0.00001),
+        ))
+    }
+}
